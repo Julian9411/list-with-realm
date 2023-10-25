2e16ec59214989ced5fa0a6c5a1c7bb8897112b9
Julian Clavijo <42986797+Julian9411@users.noreply.github.com>
2023-10-25 10:30:22 -0500

    add memoization for list component

--- components/List/index.tsx
+++ components/List/index.tsx
@@ -1,6 +1,7 @@
 import { useQuery, useRealm } from "@realm/react";
 import { FlatList, Pressable, Text, View } from "react-native";
 import { Person } from "../../scheme";
+import { useMemo } from "react";
 
 const List = () => {
   const people = useQuery(Person);
@@ -12,31 +13,34 @@ const List = () => {
     });
   };
 
-  return (
+  const list = useMemo(() => (
     <View style={{marginTop: 30}}>
-      <Text style={{ fontSize: 24, fontWeight: "bold", textAlign: "center" }}>
-        List of people
-      </Text>
-      <FlatList
-        data={people}
-        renderItem={({ item, index }) => (
-          <View
-            key={`${item.name}-${index}`}
-            style={{
-              flexDirection: "row",
-              justifyContent: "space-between",
-              paddingHorizontal: 15,
-            }}
-          >
-            <Text>{item.name}</Text>
-            <Pressable onPress={() => deleteDog(item)}>
-              <Text>X</Text>
-            </Pressable>
-          </View>
-        )}
-      />
-    </View>
-  );
+    <Text style={{ fontSize: 24, fontWeight: "bold", textAlign: "center" }}>
+      List of people
+    </Text>
+    <FlatList
+      data={people}
+      renderItem={({ item, index }) => (
+        <View
+          key={`${item.name}-${index}`}
+          style={{
+            flexDirection: "row",
+            justifyContent: "space-between",
+            paddingHorizontal: 15,
+          }}
+        >
+          <Text>{item.name}</Text>
+          <Pressable onPress={() => deleteDog(item)}>
+            <Text>X</Text>
+          </Pressable>
+        </View>
+      )}
+    />
+  </View>
+  ), [people])
+
+  return list
+  
 };
 
 export default List
